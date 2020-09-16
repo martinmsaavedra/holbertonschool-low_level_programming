ea02c515c59f8cf21e33197307e0809e7b4ac50a
martinmsaavedra <martin@bildung.uy>
2020-09-16 15:05:09 +0000

    Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - puts string
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
