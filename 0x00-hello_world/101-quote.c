9279d7dcec2ef3e572074ecc496897ab57a37f55
martinmsaavedra <martin@bildung.uy>
2020-09-16 17:42:24 +0000

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include <unistd.h>
 /**
- * main - Printf size of data types
+ * main - Write error
  * Return: Always 0 (success)
  */
 int main(void)
 {
-write(2, "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
-return (0);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
