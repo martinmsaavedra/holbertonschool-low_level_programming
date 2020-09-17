53cca95e95e739d4ce546150ff5d5b50558a785a
martinmsaavedra <martin@bildung.uy>
2020-09-17 17:45:37 +0000

    Boolean operators

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 48; i <= 57; i++)
+{
+for (j = 48; j <= 57; j++)
+{
+putchar(i);
+putchar(j);
+if (i != 57 || j != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+putchar('\n');
+return (0);
+}
