624d41512f2f86e62c48711931805f6d2fabc574
martinmsaavedra <martin@bildung.uy>
2020-09-17 16:23:18 +0000

    Boolean operators

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,11 +16,11 @@ if (d > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, d);
 }
-else if (d < 6 && d != 0)
+if (d < 6 && d != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
 }
-else
+if (n == 0)
 {
 printf("Last digit of %d is %d and is zero\n", n, d);
 }
