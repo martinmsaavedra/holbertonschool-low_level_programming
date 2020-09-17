44ca330e9ea1aae81df708b377adad5a5d2c0196
martinmsaavedra <martin@bildung.uy>
2020-09-17 18:11:45 +0000

    Boolean operators

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,11 @@ for (i = 48; i <= 57; i++)
 {
 for (j = 48; j <= 57; j++)
 {
-if(i < j)
+if (i < j)
 {
 putchar(i);
 putchar(j);
-if ( i != 56 || j != 57)
+if (i != 56 || j != 57)
 {
 putchar(44);
 putchar(32);
