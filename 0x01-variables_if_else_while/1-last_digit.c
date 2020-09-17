aff604c94a1969548bc23a81f8b0af5365aa1552
martinmsaavedra <martin@bildung.uy>
2020-09-17 14:59:00 +0000

    Boolean operators

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Evaluates random number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n, d;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+d = n % 10;
+if (d > 5)
+{
+  printf("Last digit of %d is %d and is greater than 5\n", n, d);
+}
+if (d == 0)
+{
+  printf("Last digit of %d is %d and is zero", n, d);
+}
+if (d < 6 && d != 0)
+{
+  printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int i, n;
+ for (i = 97; i <= 122; i++)
+    {
+      putchar(i);
+    }
+ for (n = 65; n <= 90; n++)
+   {
+     putchar(n);
+   }
+putchar('\n');
+return (0);
+}
