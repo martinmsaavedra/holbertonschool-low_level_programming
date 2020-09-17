88f07396d680bb44cb8a0e140fce7fc413e03aeb
martinmsaavedra <martin@bildung.uy>
2020-09-17 16:15:00 +0000

    Hello World

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-  int i, n;
- for (i = 97; i <= 122; i++)
-    {
-      putchar(i);
-    }
- for (n = 65; n <= 90; n++)
-   {
-     putchar(n);
-   }
+int i, n;
+for (i = 97; i <= 122; i++)
+{
+putchar(i);
+}
+for (n = 65; n <= 90; n++)
+{
+putchar(n);
+}
 putchar('\n');
 return (0);
 }
