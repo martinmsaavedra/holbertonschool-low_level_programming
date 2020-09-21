1a7a2f85d7d6f3e75595a179b56d8c0805eafb32
martinmsaavedra <martin@bildung.uy>
2020-09-21 22:11:29 +0000

    Functions

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+./8-24 | wc -l#include "holberton.h"
+int h1, h2, m1, m2, i, n;
+
+void jack_bauer(void)
+{
+  for (h1 = 0; h1 < 2; h1++)
+    {
+      for ( h2 = 0; h2 < ; h2++)
+	{
+	  n = h2 % 10;
+	  for (m1 = 48; m1 <= 53; m1++)
+	    {
+	      for(m2 = 48 ; m2 <= 57; m2++)
+		{
+		  if( h1 == 2 && h2 == 4)
+		    {
+		      break;
+		    }
+		  _putchar(i);
+		  _putchar(n);
+		  _putchar(':');
+		  _putchar(m1);
+		  _putchar(m2);
+		  _putchar('\n');
+		}}}}}
+
