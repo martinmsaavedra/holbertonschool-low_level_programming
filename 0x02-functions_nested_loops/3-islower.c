dc2a9404406e2a0c27cd8fa95142f987079cc7d4
martinmsaavedra <martin@bildung.uy>
2020-09-22 14:24:22 +0000

    Functions

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,11 @@
 #include "holberton.h"
+/**
+ * _islower - finds if it c is lower
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _islower(int c)
 {
 if (c >= 97 && c < 122)
