b29f904d60f63f5cea5bb576f599f34aa9ca2c09
martinmsaavedra <martin@bildung.uy>
2020-09-22 18:38:11 +0000

    Functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,21 @@
 #include "holberton.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 void print_alphabet_x10(void)
 {
-int i = 0;
- char a;
-for (; i <= 9 ; i++)
-{
-  for (a = 'a' ; a<= 'z' ; a++)
-    {
-      _putchar(a);
-    }
-  _putchar('\n');
-}
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		char a;
+
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
 }
