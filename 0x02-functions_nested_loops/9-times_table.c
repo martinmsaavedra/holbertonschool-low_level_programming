a1e1e684fc52a88b16a105667b1f8620d440e2d6
martinmsaavedra <martin@bildung.uy>
2020-09-22 21:19:22 +0000

    Functions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,33 +11,23 @@ void times_table(void)
 int a, b, res;
 for (a = 0; a < 10 ; a++)
 {
-if (a != 0)
-{
 _putchar('0');
-_putchar(',');
-}
-for (b = 0; b < 10 ; b++)
+for (b = 1; b < 10 ; b++)
 {
 res = a * b;
 if (res > 9)
 {
+_putchar(',');
 _putchar(' ');
 _putchar(res / 10 + '0');
 _putchar(res % 10 + '0');
-if (b != 9)
+}
+ else
 {
 _putchar(',');
-}}
-else if (a > 0 && b > 0)
-{
 _putchar (' ');
 _putchar (' ');
 _putchar ('0' + res);
-if (b != 9)
-{
-_putchar(',');
-}}}
-if (a != 0)
-{
+}}
 _putchar('\n');
-}}}
+}}
