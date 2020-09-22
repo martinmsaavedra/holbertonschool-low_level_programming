2e3d6238d891b2d78470dc68b00b2156f19801c0
martinmsaavedra <martin@bildung.uy>
2020-09-22 17:28:10 +0000

    Functions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,28 +1,36 @@
 #include "holberton.h"
-
+/**
+ * jack_bauer - writes the character c to stdout
+ *
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
 void jack_bauer(void)
-
 {
-  int h1, h2, m1, m2;
+int h1, h2, m1, m2;
 
-  for (h1 = 0; h1 < 3; h1++)
-    { 
-       
-for ( h2 = 0; h2 <= 9; h2++)
+for (h1 = 0; h1 < 3; h1++)
+{
+	for (h2 = 0; h2 <= 9; h2++)
 	{
-if (h1 == 2 && h2 == 4)
-	{
-	  break;
-	} 
-	  for (m1 = 0; m1 <= 5; m1++)
-	    {
-	      for(m2 = 0 ; m2 <= 9; m2++)
-		{ 
-		  _putchar(h1 + '0');
-		  _putchar(h2 + '0');
-		  _putchar(':');
-		  _putchar(m1 + '0');
-		  _putchar(m2 + '0');
-		  _putchar('\n');  		    
-		}}}}}
+		if (h1 == 2 && h2 == 4)
+		{
+			break;
+		}
+		for (m1 = 0; m1 <= 5; m1++)
+		{
+			for (m2 = 0 ; m2 <= 9; m2++)
+			{
+				_putchar(h1 + '0');
+				_putchar(h2 + '0');
+				_putchar(':');
+				_putchar(m1 + '0');
+				_putchar(m2 + '0');
+				_putchar('\n');
+			}
+		}
+	}
+}
+}
