8ced9a9564131a7041adb08bb01caf14a77e2055
martinmsaavedra <martin@bildung.uy>
2020-09-22 17:17:22 +0000

    Functions

---
+++ 0x02-functions_nested_loops/7-last_digit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_last_digit - writes the character c to stdout
+ * @n: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int print_last_digit(int n)
+{
+	int ld;
+	if (n < 0)
+	{
+		n = (n * -1);
+	}
+	ld = n % 10;
+	_putchar(ld + '0');
+	return (ld);
+}
