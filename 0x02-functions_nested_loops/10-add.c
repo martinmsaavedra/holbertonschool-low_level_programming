070efcf116ea12b00f6740c71063dd97485aef62
martinmsaavedra <martin@bildung.uy>
2020-09-22 13:49:57 +0000

    Functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - add to integers
+ * @a: int to add
+ * @b: int to add
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
