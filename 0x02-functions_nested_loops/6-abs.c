c235966690dfa54577774bd9bf6992f1bb2b2c27
martinmsaavedra <martin@bildung.uy>
2020-09-22 17:11:40 +0000

    Debugging

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
-
+/**
+ * _putchar - Prints absolute value c to stdout
+ * @n: The integer to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _abs(int n)
 {
 if (n < 0)
