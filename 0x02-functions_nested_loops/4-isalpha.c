1abe94a03621177d5c1c2df16cabf4dcf5732236
martinmsaavedra <martin@bildung.uy>
2020-09-22 17:08:57 +0000

    Debugging

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,12 @@
 #include "holberton.h"
+/**
+ * _isalpha - check if character is alphanumeric
+ * @c: The character to check
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
 int _isalpha(int c)
 {
 if ((c >= 97 && c < 122) || (c >= 65 && c <= 90))
