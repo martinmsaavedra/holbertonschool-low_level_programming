d143eed81b8f14487a4c09d4e42f5bc76fe72062
martinmsaavedra <martin@bildung.uy>
2020-09-22 12:26:14 +0000

    Functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
-   /**
-   * print_aphabet - print alphabet
-   * 
-   *
-   * Return: On success 1.
-   * On error, -1 is returned, and errno is set appropriately.
-   */
+/**
+ * print_alphabet - writes the character c to stdout
+ *
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 void print_alphabet(void)
 {
 char a;
