3be471cddd26f5952297b3c5226d8289525c7d81
martinmsaavedra <martin@bildung.uy>
2020-09-22 20:37:56 +0000

    Functions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,49 +8,31 @@
  */
 void times_table(void)
 {
-int a, b, primer_digito, segundo_digito, res;
+int a, b, res;
 for (a = 0; a < 10 ; a++)
 {
+_putchar('0');
+_putchar(',');
 for (b = 0; b < 10 ; b++)
 {
 res = a * b;
-if (res == 0 && a == 0 && b == 0)
-{
-_putchar('0');
-
-_putchar(',');
-}
-if (res == 0 && a == 0 && b != 0)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar('0');
-_putchar(',');
-}
-if (res == 0 && a != 0)
-{
-_putchar('0');
-_putchar(',');
-}
 if (res > 9)
 {
-primer_digito = res / 10;
-segundo_digito = res % 10;
 _putchar(' ');
-_putchar(primer_digito + '0');
-_putchar(segundo_digito + '0');
- if (b != 9)
-   {
-_putchar(',');
-   }}
-if (res < 10 && res != 0)
+_putchar(res / 10 + '0');
+_putchar(res % 10 + '0');
+if (b != 9)
 {
-_putchar(' ');
-_putchar(' ');
-_putchar(res + '0');
- if(b != 9)
-   {
 _putchar(',');
-   }}}
+}}
+ else
+{
+_putchar (' ');
+_putchar (' ');
+_putchar ('0' + res);
+ if (b != 9)
+{
+_putchar(',');
+ }}}
 _putchar('\n');
 }}
