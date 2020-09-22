26321b5b9305c9576bcf3d2af592d56ab8574442
martinmsaavedra <martin@bildung.uy>
2020-09-22 17:10:23 +0000

    Debugging

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,11 @@
 #include "holberton.h"
+/**
+ * print_sign - print absolute value of n to stdout
+ * @n: The int to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int print_sign(int n)
 {
 if (n > 0)
