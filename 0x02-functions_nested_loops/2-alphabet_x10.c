a890ba179f16b8c2cdbd20a7ee3bd21cbdcfd6e9
martinmsaavedra <martin@bildung.uy>
2020-09-22 17:34:08 +0000

    Functions

--- 0x02-functions_nested_loops/2-alphabet_x10.c
+++ 0x02-functions_nested_loops/2-alphabet_x10.c
@@ -8,15 +8,15 @@
  */
 void print_alphabet_x10(void)
 {
-	int i = 0;
-	char a;
+int i = 0;
+char a;
 
-	for (; i <= 9 ; i++)
-	{
-		for (a = 'a' ; a <= 'z' ; a++)
-		{
-			_putchar(a);
-		}
-		_putchar('\n');
-	}
+for (; i <= 9 ; i++)
+{
+for (a = 'a' ; a <= 'z' ; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
 }
