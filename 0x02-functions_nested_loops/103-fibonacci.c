10bdf3258786db6e4611bbc7c0ccdd4e6ed2834d
martinmsaavedra <martin@bildung.uy>
2020-09-23 00:05:20 +0000

    Nested Functions

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci numbers
+ *
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+	int a;
+	long f1, f2, fibo;
+
+	f1 = 1;
+	f2 = 2;
+	for (a = 0; a < 34; a++)
+	{
+		if (f1 % 2 == 0)
+		{
+			printf("%ld", f1);
+			if (a != 33)
+			{
+				printf(", ");
+			}}
+		fibo = f1 + f2;
+		f1 = f2;
+		f2 = fibo;
+	}
+	printf("\n");
+	return (0);
+}
