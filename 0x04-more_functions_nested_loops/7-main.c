f28df08704202521effe78a2f504eac55d6ff806
martinmsaavedra <martin@bildung.uy>
2020-09-24 17:44:25 +0000

    Functions and nested loops

--- 0x04-more_functions_nested_loops/7-main.c
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -12,18 +12,4 @@ int main(void)
 	print_diagonal(10);
 	print_diagonal(-4);
 	return (0);
-}#include "holberton.h"
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_line(0);
-	print_line(2);
-	print_line(10);
-	print_line(-4);
-	return (0);
 }
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * print_diagonal - prints a diagonal of n length to stdout
+ * @n: diagonal lenght
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n != 0 || n > 0)
+	{
+		for (a = 0 ; a < n ; a++)
+		{
+			for (b = 0 ; b <= n ; b++)
+			{
+				if (a == b)
+				{
+					_putchar(92);
+					break;
+				}
+				else
+				{
+					_putchar(32);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
