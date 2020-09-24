f174e623e7b53068883d8e49cb75ddc957781acd
martinmsaavedra <martin@bildung.uy>
2020-09-24 14:51:14 +0000

    More function and nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,7 +14,11 @@ void more_numbers(void)
 	{
 		for (b = 0 ; b <= 14 ; b++)
 		{
-				_putchar(b % 10 + '0');
+			if (b > 9)
+			{
+				_putchar(b / 10 + '0');
+			}
+		_putchar(b % 10 + '0');
 		}
 	_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * print_line - print lines of n lenght
+ * @n: lenght of line
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_line(int n)
+{
+}
