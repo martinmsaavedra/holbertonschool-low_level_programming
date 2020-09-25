d60b136348bdb4f56497ba891aa62b84d9d833c3
Martin Saavedra <45432462+martinmsaavedra@users.noreply.github.com>
2020-09-25 17:20:58 -0300

    Functions and nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,62 +1,21 @@
 #include "holberton.h"
 
 /**
- * print_number - prints integer n to stdout
- * @n: The integer to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
  */
-
-int potencia(int a);
-int cantidad_cifras(int n);
 
 void print_number(int n)
 {
-	int cant_cifras, i, digitos, unidad;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
-		n = -n;
 		_putchar('-');
+		num = -num;
 	}
-	cant_cifras = cantidad_cifras(n);
 
-	for (i = cant_cifras; i < cant_cifras; i++)
-	{
-		digitos = (n / potencia((cant_cifras - i)));
-		_putchar(digitos + '0');
-
-		if (n / potencia((cant_cifras - 1)) == 0)
-		{
-			unidad = n % 10;
-			_putchar(unidad + '0');
-		}
-	}
-}
-
-int cantidad_cifras(int n)
-{
-	int cifras, cant_cifras;
-	cifras = n;
-
-	while (cifras / 10 != 0)
-	{
-		cifras = n / 10;
-		n = cifras;
-		cant_cifras++;
-	}
-	return (cant_cifras);
-}
-
-int potencia(int n)
-{
-	int i;
-	unsigned int pow = 10;
-
-	for (i = 0; i < n - 1; i++)
-	{
-		pow = pow * 10;
-	}
-	return (pow);
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
 }
