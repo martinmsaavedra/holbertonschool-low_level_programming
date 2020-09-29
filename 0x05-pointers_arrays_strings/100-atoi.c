49111af9d56fe0b09b4d3b2e861a73a228ccf29b
martinmsaavedra <martin@bildung.uy>
2020-09-29 17:30:25 +0000

    Pointers...

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -22,7 +22,7 @@ int _atoi(char *s)
 
 	while (s[i] != '\0' && s[i] >=  '0' && s[i] <= '9')
 	{
-		res = (res * 10) - (s[i] - '0');
+		res = (res * 10) - (s[i++] - '0');
 	}
 
 	return (res * sign);
