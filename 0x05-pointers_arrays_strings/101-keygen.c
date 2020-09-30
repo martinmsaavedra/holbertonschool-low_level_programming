470f6e5c183137ab6cb54f21e88225919eea2a59
martinmsaavedra <martin@bildung.uy>
2020-09-30 20:05:55 +0000

    Pointers...

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,21 +11,22 @@
 int main(void)
 {
 	int i = 0;
-	int contador = 2772;
+	int contador = 0;
 	int letra;
 	char pass[100];
+	int resto;
 
 	srand(time(NULL));
 
-	while (contador > 127)
+	while (contador < 2645)
 	{
-		letra = (rand() % 94) + 33;
+		letra = (rand() % 25) + 97;
 		pass[i] = letra;
-		contador = contador - pass[i];
+		contador = contador + pass[i];
 		i++;
 	}
-	pass[i] = contador;
+	resto = 2772 - contador;
+	pass[i++] = resto;
 	pass[i++] = '\0';
-	printf("%s\n", pass);
 	return (0);
 }
