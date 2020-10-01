82b0ed76005c7f416b5dc83ed3b561f38f4b4c74
martinmsaavedra <martin@bildung.uy>
2020-10-01 19:12:57 +0000

    More pointers...

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *print_number - print an integer
+ *@n: the integer
+ **/
+
+void print_number(int n)
+{
+	unsigned long num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	if ((num / 10) > 0)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}
