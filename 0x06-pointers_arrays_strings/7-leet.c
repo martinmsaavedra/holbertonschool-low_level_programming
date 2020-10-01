d57e26c15d7d6ea183503d130a41fec2cc40bb5c
martinmsaavedra <martin@bildung.uy>
2020-10-01 18:17:17 +0000

    More pointers...

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,16 +8,18 @@
 char *leet(char *s)
 {
 	int i, j;
-	char letras[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	char numumeros[11] = {"4", "4", "3", "3", "0", "0", "7", "7", "1", "1"};
+	char letras[] = "aAeEoOtTlL";
+	char numeros[] = "4433007711";
 
-	for (i = 0; s[0] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; s[0] != '\0'; j++)
+		for (j = 0; letras[j] != '\0'; j++)
 		{
-			if (s[j] == letras[i])
-				s[j] == numeros[i]
-					}
+			if (s[i] == letras[j])
+			{
+				s[i] = numeros[j];
+			}
+		}
 	}
-	return (s)
+	return (s);
 }
