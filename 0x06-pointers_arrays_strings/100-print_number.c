526901fa4c761088d9fc93c934b0ee7394a3ddca
martinmsaavedra <martin@bildung.uy>
2020-10-01 19:14:51 +0000

    More pointers...

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,21 +1,21 @@
 #include "holberton.h"
+
 /**
- *print_number - print an integer
- *@n: the integer
- **/
+ * print_number - Prints integers.
+ * @n: Integer to be printed.
+ */
 
 void print_number(int n)
 {
-	unsigned long num = n;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -n;
+		num = -num;
 	}
+
 	if ((num / 10) > 0)
-	{
 		print_number(num / 10);
-	}
 	_putchar((num % 10) + '0');
 }
