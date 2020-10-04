37de2e598420f53d19c8bcbf13b8f1db0a176d9e
martinmsaavedra <martin@bildung.uy>
2020-10-04 15:28:05 +0000

    More pointers...

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -2,35 +2,34 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- *
- *
+ *print_buffer - print buffer
+ *@b: string
+ *@size: buffer size
  */
 
 void print_buffer(char *b, int size)
 {
-	int i;
-	int j;
-	int count = 0;
-	char *buffer = b;
-	if ( size > 0) {
-
-for ( i = 0; i < size; i = i + 10)
+int i;
+int j;
+int count = 0;
+char *buffer = b;
+if (size > 0)
+{
+	for (i = 0; i < size; i = i + 10)
 	{
-		// Output the offset.
 		printf(" %08x: ", count);
-
 		if (buffer[i] >= 0 && buffer[i] <= 32)
 		{
 			buffer[i] = '.';
 		}
-		    if(buffer[i] >= 33 && buffer[i] <= 255 || buffer[i] != 00)
+		if (buffer[i] >= 33 && buffer[i] <= 255 || buffer[i] != 00)
 		{
 			printf("%02x%02x %02x%02x %02x%02x %02x%02x %02x%02x ",
-			       buffer[i], buffer[i + 1], buffer[i + 2], buffer[i + 3], 
-			       buffer[i + 4], buffer[i + 5], buffer[i + 6], buffer[i + 7], 
-			       buffer[i + 8], buffer[i + 9]);
+buffer[i], buffer[i + 1], buffer[i + 2], buffer[i + 3],
+buffer[i + 4], buffer[i + 5], buffer[i + 6],
+buffer[i + 7], buffer[i + 8], buffer[i + 9]);
 		}
-		if(buffer[i] == 00)
+		if (buffer[i] == 00)
 		{
 			printf("00");
 		}
@@ -41,7 +40,9 @@ for ( i = 0; i < size; i = i + 10)
 		count += 10;
 		printf("\n");
 	}
-	}
-	else
-		printf("\n");
+}
+else
+{
+printf("\n");
+}
 }
