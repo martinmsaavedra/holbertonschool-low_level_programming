038c98f87fac4afe10eac5ac128c3753a95af0d9
martinmsaavedra <martin@bildung.uy>
2020-10-04 16:14:36 +0000

    More pointers...

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -8,24 +8,21 @@
  */
 void print_buffer(char *b, int size)
 {
-	int i, int j, int count = 0, char *buffer = b;
+	int i;
+	int j;
+	int count = 0;
+	char *buffer = b;
 
 	if (size > 0)
 	{
 		for (i = 0; i < size; i = i + 10)
 		{
 			printf(" %08x: ", count);
-			if (buffer[i] >= 33 && buffer[i] <= 255 || buffer[i] != 00)
-			{
-			printf("%02x%02x %02x%02x %02x%02x %02x%02x %02x%02x ",
-buffer[i], buffer[i + 1], buffer[i + 2], buffer[i + 3],
-buffer[i + 4], buffer[i + 5], buffer[i + 6],
-buffer[i + 7], buffer[i + 8], buffer[i + 9]);
-			}
-			if (buffer[i] == 00)
-			{
-				printf("00");
-			}
+			printf("%02x%02x ", buffer[i], buffer[i + 1]);
+			printf("%02x%02x ", buffer[i + 2], buffer[i + 3]);
+			printf("%02x%02x ", buffer[i + 4], buffer[i + 5]);
+			printf("%02x%02x ", buffer[i + 6], buffer[i + 7]);
+			printf("%02x%02x ", buffer[i + 8], buffer[i + 9]);
 			for (j = count; j < count + 10; j++)
 			{
 				if (isprint(buffer[j]))
@@ -39,11 +36,10 @@ buffer[i + 7], buffer[i + 8], buffer[i + 9]);
 			}
 			count += 10;
 			printf("\n");
+		}
 	}
-}
 	else
 	{
 		printf("\n");
 	}
-
 }
