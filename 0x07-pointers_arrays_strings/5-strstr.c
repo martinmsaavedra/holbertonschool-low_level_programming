58bfab6fc7dc23f05ec7732ec99c5923e7fbe217
martinmsaavedra <martin@bildung.uy>
2020-10-05 15:40:37 +0000

    Even more pointers...

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *_strstr - locates a substring
+ *@haystack: string where find
+ *@needle: sting to find
+ *Return: pointer were string was founded
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[0])
+				return (&haystack[i]);
+		}
+	}
+	return (0);
+}
