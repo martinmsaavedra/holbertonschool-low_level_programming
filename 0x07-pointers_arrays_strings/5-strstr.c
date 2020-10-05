56c94b56ce7d0d0260d3ba1d0455e66806ba1eeb
martinmsaavedra <martin@bildung.uy>
2020-10-05 20:11:19 +0000

    Even more pointers...

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,6 +10,9 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
+	if (needle == NULL)
+		return (haystack);
+
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		if (needle[0] == haystack[i])
