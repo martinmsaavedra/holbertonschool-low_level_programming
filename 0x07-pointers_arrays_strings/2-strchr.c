57163d34cc4ed73a3974bafdc45542a31b44559d
martinmsaavedra <martin@bildung.uy>
2020-10-05 15:20:33 +0000

    Even more pointers...

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,13 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 		{
 			return (&s[i]);
 		}
+		i++;
 	}
 	return (0);
 }
