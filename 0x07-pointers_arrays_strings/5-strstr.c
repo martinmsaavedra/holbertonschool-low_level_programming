cb365c862e3ec9c5823afce2b6ee3c8981e92d78
martinmsaavedra <martin@bildung.uy>
2020-10-05 17:59:51 +0000

    Even more pointers...

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,10 +12,16 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		if (needle[0] == haystack[i])
 		{
-			if (haystack[i] == needle[0] && haystack[i] != '\0' && needle[j] != '\0')
-				return (&haystack[i]);
+			for (j = 1; needle[j] != '\0'; j++)
+			{
+				if (!(needle[j] == haystack[i + j]))
+				{
+					break;
+				}
+			return (&haystack[i]);
+			}
 		}
 	}
 	return (0);
