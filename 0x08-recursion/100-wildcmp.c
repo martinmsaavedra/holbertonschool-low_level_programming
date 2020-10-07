d242976f60c30e99efc697b7cc241373123b67c9
martinmsaavedra <martin@bildung.uy>
2020-10-07 21:34:34 +0000

    recursion

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -9,6 +9,8 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s2 == '*')
 		return (aux2(s1, s2));
+	if (*s2 == '\0' && *s1 != '\0')
+		return (0);
 
 	if (*s1 == '\0' || *s2 == '\0')
 	{
@@ -28,12 +30,22 @@ int wildcmp(char *s1, char *s2)
  */
 int aux2(char *s1, char *s2)
 {
+	if (*s1 == *s2)
+		return (wildcmp(s1, s2));
+	if (*s2 == '\0')
+		return (aux2(s1 + 1, s2));
 	if (*(s2 + 1) == '\0')
 		return (1);
 	s2++;
+
+	if (*s2 + 1 == *s1)
+		return (wildcmp(s1, s2 + 1));
+	else if ((*s2 + 1) == '*')
+		return (aux2(s1, s2 + 1));
+	else if (*(s2 + 1) != '*' && *(s2 + 1) != '\0')
+		return (aux2(s1 + 1, s2));
+
 	if (*s1 == '\0')
 		return (wildcmp(s1, s2));
 	return (aux2(s1 + 1, s2));
-	if (*s1 == *s2)
-		return (wildcmp(s1, s2));
 }
