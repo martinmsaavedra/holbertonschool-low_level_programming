a55a68db2e784d762c04a0bd2ed29ac08cec910b
martinmsaavedra <martin@bildung.uy>
2020-10-11 01:28:52 +0000

    argc argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,11 +17,10 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return (1);	
+		return (1);
 	}
 	else
 	{
-		
 		a = atoi(argv[1]);
 		b = atoi(argv[2]);
 		printf("%d\n", a * b);
