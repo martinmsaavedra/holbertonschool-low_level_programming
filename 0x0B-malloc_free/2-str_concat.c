2c769bacddb714524981b4f494289e3c6f99402b
martinmsaavedra <martin@bildung.uy>
2020-10-14 18:10:12 +0000

    Malloc and Free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 #include <stdlib.h>
-#include <stdio.h>
 /**
  * str_concat - concatenates two strings
  *@s1: pointer to string 1
@@ -11,27 +10,22 @@ char *str_concat(char *s1, char *s2)
 {
 	int c1 = 0;
 	int c2 = 0;
-	int i = 0;
-	int j = 0;
+	int i, j;
 	char *parray;
 
-	if (s1[0] == NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
 	}
-	if (s2[0] == NULL)
+	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-	while (s1[i] != '\0')
+	for (i = 0; s1[i] != '\0', c1++, i++)
 	{
-		c1++;
-		i++;
 	}
-	while (s2[j] != '\0')
+	for (j = 0; s2[j] != '\0', c2++, j++)
 	{
-		c2++;
-		j++;
 	}
 	parray = (char *) malloc((c1 + c2 + 2));
 	if (parray == NULL)
