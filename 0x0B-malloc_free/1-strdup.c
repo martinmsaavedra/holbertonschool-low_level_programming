96376b1ba889b6fca08783586ca364b8dd13d2ac
martinmsaavedra <martin@bildung.uy>
2020-10-14 16:28:13 +0000

    malloc and free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strlen - calculate the length of a string
+ * @s: pointer of string
+ *
+ * Return: returns the length of a string
+ */
+int _strlen(char *s)
+{
+	int contador = 0;
+	int total = 0;
+
+	while (*(s + contador) != '\0')
+	{
+		contador++;
+		total++;
+	}
+	return (total);
+}
+
+
+/**
+ *_strdup - duplicates a string
+ *@str: size of array
+ *Return: pointer to string or 0 if size is 0
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	char *parray;
+	int size;
+
+	size = _strlen(str);
+
+	if (size != 0)
+	{
+		parray = (char *)malloc(sizeof(char) * size);
+		if (parray != NULL)
+		{
+			for (i = 0; i < size; i++)
+				parray[i] = str[i];
+			return (parray);
+		}
+		else
+			return (NULL);
+	}
+	else
+		return (NULL);
+}
