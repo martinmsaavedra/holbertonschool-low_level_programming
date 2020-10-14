c9b914e02c41984e0aa8a60fe8a0e563ac4e9280
martinmsaavedra <martin@bildung.uy>
2020-10-14 17:13:58 +0000

    Malloc and Free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,28 +1,9 @@
 #include "holberton.h"
 #include <stdlib.h>
-/**
- * _strlen - calculate the length of a string
- * @s: pointer of string
- *
- * Return: returns the length of a string
- */
-int _strlen(char *s)
-{
-	int contador = 0;
-	int total = 0;
-
-	while (*(s + contador) != '\0')
-	{
-		contador++;
-		total++;
-	}
-	return (total);
-}
-
 
 /**
  *_strdup - duplicates a string
- *@str: size of array
+ *@str: string
  *Return: pointer to string or 0 if size is 0
  */
 
@@ -30,18 +11,27 @@ char *_strdup(char *str)
 {
 	int i;
 	char *parray;
-	int size;
+	int size = 0;
+	int j = 0;
 
-	size = _strlen(str);
-
-	if (size == 0)
+	if (str == 0)
+	{
 		return (NULL);
+	}
 	else
 	{
-parray = (char *)malloc(sizeof(char) * size + 1);
+		while (str[j] != '\0')
+	{
+		size++;
+		j++;
+	}
+		parray = (char *)malloc(sizeof(char) * size + 1);
 	if (parray == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
+	{
 		parray[i] = str[i];
+	}
 	return (parray);
-	}}
+	}
+}
