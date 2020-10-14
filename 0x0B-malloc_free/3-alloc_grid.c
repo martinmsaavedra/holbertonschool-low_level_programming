b62b164e0118430ebbaefd9fa20e05313e8b2e46
martinmsaavedra <martin@bildung.uy>
2020-10-14 19:25:44 +0000

    Malloc and Free

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - prints a grid of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: pointer to two dimensional array
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **parray;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		parray = (int **)malloc(height * sizeof(int *));
+
+		for (i = 0; i < height; i++)
+		{
+			parray[i] = (int *) malloc(height * sizeof(int));
+		}
+		for (i = 0; i < height; i++)
+		{
+			for (j = 0; j < width; j++)
+			{
+				parray[i][j] = 0;
+			}
+		}
+		return (parray);
+	}
+}
