99114792c9f1ddbd2495f0b10f96524414ff1d93
martinmsaavedra <martin@bildung.uy>
2020-10-14 17:03:58 +0000

    Malloc and Free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -34,15 +34,12 @@ char *_strdup(char *str)
 
 	size = _strlen(str);
 
-	if (size != 0)
-	{
-		parray = (char *)malloc(sizeof(char) * size + 1);
-		if (parray == NULL)
-			return (NULL);
-		for (i = 0; i < size; i++)
-			parray[i] = str[i];
-		return (parray);
-	}
-	else
+	if (size == 0)
 		return (NULL);
+	parray = (char *)malloc(sizeof(char) * size + 1);
+	if (parray == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		parray[i] = str[i];
+	return (parray);
 }
