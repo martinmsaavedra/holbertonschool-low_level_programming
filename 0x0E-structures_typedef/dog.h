f882dd52e921de5ff961e045a53941f3f66c2964
martinmsaavedra <martin@bildung.uy>
2020-10-19 17:37:55 +0000

    Structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,10 +13,5 @@ struct dog
 	float age;
 	char *owner;
 };
-#endif
-
-#ifndef INIT_DOG
-#define INIT_DOG
-#define INIT_DOG
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
