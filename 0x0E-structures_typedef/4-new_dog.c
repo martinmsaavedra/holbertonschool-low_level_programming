4448cfb4ecda6dde9feb258e6ecc1c4efec9ff4c
martinmsaavedra <martin@bildung.uy>
2020-10-19 19:22:42 +0000

    Structures

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,80 @@
+#include "dog.h"
+#include <stdlib.h>
+char *_strdup(char *str);
+/**
+ *new_dog - creates a new dog
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ *Return: pointer to new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *name_;
+	char *owner_;
+
+	name_ = _strdup(name);
+	owner_ = _strdup(owner);
+
+	if ((!name) || (!owner))
+		return (NULL);
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	name_ = _strdup(name);
+	if (!name_)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	owner_ = _strdup(owner);
+	if (!owner_)
+	{
+		free(new_dog);
+		free(name_);
+		return (NULL);
+	}
+
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+
+	return (new_dog);
+}
+/**
+ *_strdup - duplicates a string
+ *@str: string
+ *Return: pointer to string or 0 if size is 0
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *parray;
+	int size = 0;
+	int j = 0;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (str[j] != '\0')
+		{
+			size++;
+			j++;
+		}
+		parray = (char *)malloc(sizeof(char) * size + 1);
+		if (parray == NULL)
+			return (NULL);
+		for (i = 0; i < size; i++)
+		{
+			parray[i] = str[i];
+		}
+		return (parray);
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -28,4 +28,6 @@ typedef struct dog_t
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+char *_strdup(char *str);
 #endif
