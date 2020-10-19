65b662d1a0abde009836c3c41bf8c72b6c3a28e2
martinmsaavedra <martin@bildung.uy>
2020-10-19 18:23:08 +0000

    Structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,6 +13,19 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ *struct dog_t - dog structure
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ */
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
