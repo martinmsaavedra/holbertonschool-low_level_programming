b89411e451e8da57bad5af90d687ba5d09cd4d91
martinmsaavedra <martin@bildung.uy>
2020-10-21 16:41:22 +0000

    Function Pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,10 +11,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (!array || !cmp)
 		return (0);
-	for (i = 0; i < size, i++)
+	for (i = 0; i < size; i++)
 	{
-		if (!(cmp(array[i])))
+		if (cmp(array[i]) != 0)
 			return (i);
 	}
+	return (0);
 
 }
