5e1d06c312b45300ba10c9b2d607431b29ef6805
martinmsaavedra <martin@bildung.uy>
2020-10-21 16:25:51 +0000

    Function Pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,3 @@
-#include "function_pointers.h"
 /**
  *print_name - function that prints a name
  *@name: name string
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,2 +1,5 @@
+#ifndef FUNCTIONS
+#define FUNCTIONS
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+#endif
