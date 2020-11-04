53294e9962528b9a535e2fcb2791a24c2f850be1
martinmsaavedra <martin@bildung.uy>
2020-11-04 13:32:48 +0000

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,7 +8,6 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *aux = *head;
 	listint_t *new;
 
 	if (!head)
@@ -22,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new->n = n;
-	new->next = aux->next;
-	aux->next = new;
+	new->next = *head;
+	*head = new;
 	return (new);
 }
