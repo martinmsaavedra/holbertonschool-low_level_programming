dfa5246c4ef85a7cecf8e2e2883f5740ac2a1cf3
martinmsaavedra <martin@bildung.uy>
2020-11-04 17:16:08 +0000

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,36 +6,54 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
+
 	int loop = 0;
 	unsigned int number = 0;
-	const listint_t *sp = head, *fp = head;
+	const listint_t *sp = head, *fp = head, *hl = NULL;
 
 	if (!head)
 		exit(98);
 
 	while (fp)
-{
-	printf("[%p] %d\n", (void *)sp, sp->n);
-	sp = sp->next;
-	fp = fp->next->next;
-	number++;
-	if (sp == fp)
 	{
-		loop = 1;
-		break;
+		sp = sp->next;
+		fp = fp->next->next;
+		number++;
+		if (sp == fp)
+		{
+			loop = 1;
+			break;
+		}
 	}
-}
 	if (loop == 1)
 	{
 		sp = head;
 		while (fp != sp)
 		{
-			printf("[%p] %d\n", (void *)fp, fp->n);
 			sp = sp->next;
 			fp = fp->next;
 			number++;
 		}
-	 printf("->[%p] %d\n", (void *)sp, sp->n);
+		sp = head;
+		while (number > 0)
+		{
+
+			printf("[%p] %d\n", (void *)sp, sp->n);
+			sp = sp->next;
+			number--;
+			hl = sp;
+		}
+		printf("->[%p] %d\n", (void *)hl, hl->n);
 	}
-	return (number);
+	else
+	{
+		sp = head;
+		while (sp)
+		{
+
+			printf("[%p] %d\n",(void *)sp, sp->n);
+			sp = sp->next;
+		}
+	}
+return (number);
 }
