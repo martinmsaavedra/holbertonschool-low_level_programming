cc4d4aae48b70cc40cef9d309a67c96093f110d2
martinmsaavedra <martin@bildung.uy>
2020-11-04 17:44:24 +0000

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 /**
+ *loopexist - check if loop exist whitin list
+ *@head: head of list
+ *@pl: pointer to loop value
+ *@pnumber: pointer to number
+ *Return: pointer to fast loop
+ */
+const listint_t *loopexist(const listint_t *head, int *pl, int *pnumber);
+/**
  *print_listint_safe - prints all the elements of a listint
  *@head: head of list
  *Return: number of elements
@@ -7,24 +15,14 @@
 size_t print_listint_safe(const listint_t *head)
 {
 
-	int loop = 0;
-	unsigned int number = 0;
+	int loop = 0, *pl = &loop;
+	int number = 0, *pnumber = &number;
 	const listint_t *sp = head, *fp = head, *hl = NULL;
 
 	if (!head)
 		exit(98);
+	fp = loopexist(head, pl, pnumber);
 
-	while (fp)
-	{
-		sp = sp->next;
-		fp = fp->next->next;
-		number++;
-		if (sp == fp)
-		{
-			loop = 1;
-			break;
-		}
-	}
 	if (loop == 1)
 	{
 		sp = head;
@@ -37,7 +35,6 @@ size_t print_listint_safe(const listint_t *head)
 		sp = head;
 		while (number > 0)
 		{
-
 			printf("[%p] %d\n", (void *)sp, sp->n);
 			sp = sp->next;
 			number--;
@@ -50,10 +47,34 @@ size_t print_listint_safe(const listint_t *head)
 		sp = head;
 		while (sp)
 		{
-
-			printf("[%p] %d\n",(void *)sp, sp->n);
+			printf("[%p] %d\n", (void *)sp, sp->n);
 			sp = sp->next;
 		}
 	}
 return (number);
+}
+/**
+ *loopexist - check if loop exist whitin list
+ *@head: head of list
+ *@pl: pointer to loop value
+ *@pnumber: pointer to number
+ *Return: pointer to fast loop
+ */
+const listint_t *loopexist(const listint_t *head, int *pl, int *pnumber)
+{
+	const listint_t *fp = head, *sp = head;
+
+	while (fp)
+	{
+		sp = sp->next;
+		fp = fp->next->next;
+		(*pnumber)++;
+		if (sp == fp)
+		{
+			*pl = 1;
+			return (fp);
+		}
+	}
+	*pl = 0;
+	return (NULL);
 }
