cf60a90591db6ab8691e4b8eeee517925a0f9c2b
martinmsaavedra <martin@bildung.uy>
2020-11-04 18:38:31 +0000

    task 14

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,37 +1,43 @@
 #include "lists.h"
+#include <stdbool.h>
 /**
- *find_listint_loop - find loops in listint
- *@head: head of list
- *Return: address of start of loop
+ * find_listint_loop - finds the loop in a linked list
+ * @head: pointer to a linked list
+ * Return: the address of where the loop starts
+ * otherwise NULL
  */
+
 listint_t *find_listint_loop(listint_t *head)
 {
-	int loop = 0;
-	const listint_t *sp = head, *fp = head;
+	listint_t *fast_ptr, *slow_ptr;
+	bool is_loop;
 
-	if (!head)
-		exit(98);
-
-	while (fp)
-{
-	sp = sp->next;
-	fp = fp->next->next;
-	if (sp == fp)
-	{
-		loop = 1;
-		break;
-	}
-}
-	if (loop == 1)
-	{
-		sp = head;
-		while (fp != sp)
-		{
-			sp = sp->next;
-			fp = fp->next;
-		}
-		return (sp);
-	}
-	else
+	if (head == NULL)
 		return (NULL);
+	fast_ptr = head;
+	slow_ptr = head;
+	is_loop = false;
+
+	while (fast_ptr && slow_ptr && fast_ptr->next)
+	{
+		fast_ptr = fast_ptr->next->next;
+		slow_ptr = slow_ptr->next;
+
+		if (slow_ptr == fast_ptr)
+		{
+			is_loop = true;
+			break;
+		}
+	}
+	if (is_loop)
+	{
+		fast_ptr = head->next;
+		slow_ptr = head;
+	}
+	while (fast_ptr && fast_ptr < slow_ptr && is_loop)
+	{
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next;
+	}
+	return (fast_ptr);
 }
