8a2f96ca0e0d5c18cc51be135c0d87ce1b8f7651
martinmsaavedra <martin@bildung.uy>
2020-11-04 21:47:08 +0000

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,12 +1,5 @@
 #include "lists.h"
-/**
- *loopexist - check if loop exist whitin list
- *@head: head of list
- *@pl: pointer to loop value
- *@pnumber: pointer to number
- *Return: pointer to fast loop
- */
-const listint_t *loopexist(const listint_t *head, int *pl, int *pnumber);
+#include <stdbool.h>
 /**
  *print_listint_safe - prints all the elements of a listint
  *@head: head of list
@@ -14,68 +7,50 @@ const listint_t *loopexist(const listint_t *head, int *pl, int *pnumber);
  */
 size_t print_listint_safe(const listint_t *head)
 {
+const	listint_t *fast_ptr, *slow_ptr;
 
-	int loop = 0, *pl = &loop;
-	int number = 0, *pnumber = &number, node = 0;
-	const listint_t *sp = head, *fp = head, *hl = NULL;
+	bool is_loop;
+	int nodes = 0;
 
 	if (head == NULL)
-	{
 		exit(98);
-	}
-	fp = loopexist(head, pl, pnumber);
-	if (loop == 1)
-	{
-		sp = head;
-		while (fp != sp)
-		{
-			sp = sp->next;
-			fp = fp->next;
-			number++;
-		}
-		sp = head;
-		node = number;
-		while (node > 0)
-		{
-			printf("[%p] %d\n", (void *)sp, sp->n);
-			sp = sp->next;
-			node--;
-			hl = sp;
-		}
-		printf("-> [%p] %d\n", (void *)hl, hl->n);
-	}
-	else
-	{
-		while (head)
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-		}
-	}
-	return (number);
-}
-/**
- *loopexist - check if loop exist whitin list
- *@head: head of list
- *@pl: pointer to loop value
- *@pnumber: pointer to number
- *Return: pointer to fast loop
- */
-const listint_t *loopexist(const listint_t *head, int *pl, int *pnumber)
-{
-	const listint_t *fp = head, *sp = head;
+	fast_ptr = head;
+	slow_ptr = head;
+	is_loop = false;
 
-	while (fp)
+	while (fast_ptr && slow_ptr && fast_ptr->next)
 	{
-		sp = sp->next;
-		fp = fp->next->next;
-		(*pnumber)++;
-		if (sp == fp)
+		fast_ptr = fast_ptr->next->next;
+		slow_ptr = slow_ptr->next;
+		nodes++;
+		if (slow_ptr == fast_ptr)
 		{
-			*pl = 1;
-			return (fp);
+			is_loop = true;
+			break;
 		}
 	}
-	*pl = 0;
-	return (NULL);
+	if (is_loop)
+	{
+		fast_ptr = head->next;
+		slow_ptr = head;
+	}
+	while (fast_ptr && slow_ptr > fast_ptr + 1 && is_loop)
+	{
+		printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next;
+	}
+	if (is_loop)
+		printf("-> [%p] %d\n", (void*)slow_ptr->next, slow_ptr->next->n);
+	if (!is_loop)
+	{
+		slow_ptr = head;
+		while (slow_ptr)
+		{
+			printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+			slow_ptr = slow_ptr->next;
+		}
+	}
+
+	return (nodes);
 }
