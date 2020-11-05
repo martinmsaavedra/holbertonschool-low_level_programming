5fb4a53d455e6723736dc0c7f3ca1d05ab7f2820
martinmsaavedra <martin@bildung.uy>
2020-11-05 16:32:40 +0000

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ *get_bit - returns the value of a bit at a given index
+ *@n: integer given
+ *@index: index of the integer in binary
+ *Return: value of a bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int len = 1;
+	unsigned long int *pn = &len;
+
+	len_binary(n, pn);
+	if (index > len)
+		return (-1);
+	if ((n >> index) & 1)
+		return (1);
+	else
+		return (0);
+
+}
+/**
+ *len_binary - lenght of binary number
+ *@n: integer to convert
+ *@pn: pointer to len
+ */
+void len_binary(unsigned long int n, unsigned long int *pn)
+{
+	if (n >> 1)
+		len_binary(n >> 1, pn);
+	*pn += 1;
+}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -5,4 +5,6 @@
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
 void print_binary(unsigned long int n);
+void len_binary(unsigned long int n, unsigned long int *);
+int get_bit(unsigned long int n, unsigned int index);
 #endif
