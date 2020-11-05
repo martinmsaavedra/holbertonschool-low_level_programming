fe91b75b8c050898043586181f6e15f191ad8256
martinmsaavedra <martin@bildung.uy>
2020-11-05 16:47:29 +0000

    update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,22 +13,10 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (n == 0 && index > 0)
 		return (-1);
 	len_binary(n, pn);
-	if (index > len)
+	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
 	if ((n >> index) & 1)
 		return (1);
 	else
 		return (0);
-
-}
-/**
- *len_binary - lenght of binary number
- *@n: integer to convert
- *@pn: pointer to len
- */
-void len_binary(unsigned long int n, unsigned long int *pn)
-{
-	if (n >> 1)
-		len_binary(n >> 1, pn);
-	*pn += 1;
 }
