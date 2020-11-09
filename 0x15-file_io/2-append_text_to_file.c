c3476b5c966eaee5cdbf549032d971fc817b7a37
martinmsaavedra <martin@bildung.uy>
2020-11-09 18:46:34 +0000

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,7 +7,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fo, fd, i = 0;
+	int fo, fw, i = 0;
 
 	if (!filename)
 		return (-1);
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		while (text_content[i] != '\0')
 			i++;
-		fd = write(fo, text_content, i);
+		fw = write(fo, text_content, i);
 		if (fd == -1)
 			return (-1);
 	}
