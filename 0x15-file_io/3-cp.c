ffacd1930c3581d32ef8cdf92b80d1d990d9ec18
martinmsaavedra <martin@bildung.uy>
2020-11-09 22:25:22 +0000

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,11 +6,11 @@
  *@av: argument vector
  *Return: 1 on success, -1 on failure
  */
-int main(int ac, char *av[])
+int main(int ac, char **av)
 {
 	char *buffer;
 	int fc1, fc2, fo1, fo2, fw;
-	ssize_t numRead = 1;
+	int numRead = 1;
 
 	if (ac != 3)
 	{
@@ -21,13 +21,13 @@ int main(int ac, char *av[])
 	fo1 = open(av[1], O_RDONLY);
 	if (fo1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	fo2 = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fo2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 	buffer = malloc(sizeof(char) * BUFFER_SIZE);
@@ -39,7 +39,8 @@ int main(int ac, char *av[])
 		numRead = read(fo1, buffer, BUFFER_SIZE);
 		if (numRead == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s", av[1]);
+			free(buffer);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
 		if (numRead > 0)
@@ -47,22 +48,22 @@ int main(int ac, char *av[])
 			fw = write(fo2, buffer, numRead);
 			if (fw == -1)
 			{
-				dprintf(STDERR_FILENO, "Error: Can't read from file %s", av[2]);
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 				free(buffer);
 				exit(99);
 			}
 		}
-}
+	}
 	fc1 = close(fo1);
 	if (fc1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i", fo1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fo1);
 		exit(100);
 	}
 	fc2 = close(fo2);
 	if (fc2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i", fo2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fo2);
 		exit(100);
 	}
 	free(buffer);
