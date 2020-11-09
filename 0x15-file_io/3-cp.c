8eccf9cc7e045cd0fefaec15231b5192f7971157
martinmsaavedra <martin@bildung.uy>
2020-11-09 22:27:22 +0000

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,18 +17,18 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	fo2 = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+        if (fo2 == -1)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+                exit(99);
+        }
 
 	fo1 = open(av[1], O_RDONLY);
 	if (fo1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
-	}
-	fo2 = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (fo2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
 	}
 	buffer = malloc(sizeof(char) * BUFFER_SIZE);
 	if (!buffer)
