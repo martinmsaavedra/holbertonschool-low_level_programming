ffae9793b4dcff812a219085615b813ca35a8ccf
martinmsaavedra <martin@bildung.uy>
2020-11-09 18:01:08 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,9 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (fo == -1)
 		return (-1);
 	if (i == 0)
-		fo = open(filename, O_TRUNC | O_RDWR, 0600);
-        if (fo == -1)
-                return (-1);
+		write(fo, NULL, i);
 	else
 		write(fo, text_content, i);
 	close(fo);
