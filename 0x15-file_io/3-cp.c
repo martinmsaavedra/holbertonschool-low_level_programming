bb136e8c0a11cf654df0a95b69ccb945b3658868
martinmsaavedra <martin@bildung.uy>
2020-11-10 00:57:03 +0000

    2157

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,16 +23,12 @@ int main(int ac, char **av)
 		exit(98);
 	}
 	fo2 = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	if (fo2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(99);
-	}
 	while ((numRead = read(fo1, buffer, 1024)) > 0)
 	{
 		if (fo2 < 0 || write(fo2, buffer, numRead) != numRead)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			close(fo1);
 			exit(99);
 		}
 	}
