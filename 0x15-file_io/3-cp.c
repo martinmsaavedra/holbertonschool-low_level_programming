b0830f6275d4fda3b166d0048b3efe43f52b65e4
martinmsaavedra <martin@bildung.uy>
2020-11-10 00:20:04 +0000

    Fix 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,7 @@
  */
 int main(int ac, char **av)
 {
-	char *buffer;
+	char buffer[1024];
 	int fc1, fc2, fo1, fo2;
 	int numRead = 1;
 
@@ -16,34 +16,26 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fo1 = open(av[1], O_RDONLY);
 	if (fo1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	fo2 = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	fo2 = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
         if (fo2 == -1)
         {
                 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
                 exit(99);
         }
-	buffer = malloc(sizeof(char) * 1024);
-	if (!buffer)
-		return (0);
-
 	while ((numRead = read(fo1, buffer, 1024)) > 0)
 	{
 		if (fo2 < 0 || write(fo2, buffer, numRead) != numRead)
 		{
-			free(buffer);
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-			exit(98);
+			exit(99);
 		}
-
 	}
-
 	if (numRead < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
@@ -61,6 +53,5 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fc2);
 		exit(100);
 	}
-	free(buffer);
-	return (1);
+	return (0);
 }
