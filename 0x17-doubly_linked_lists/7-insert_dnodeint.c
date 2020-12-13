0f8ba3dc0bee43fa78629cd7be868544b5e40d5c
martinmsaavedra <martin@bildung.uy>
2020-12-13 16:43:07 +0000

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,29 +12,29 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *aux = NULL, *new = NULL;
 	unsigned int i = 0, count = 0;
 
+	if (!h)
+		return (NULL);
+
 	aux = *h;
 	while (aux != NULL)
 	{
 		aux = aux->next;
 		count++;
 	}
-	if (idx == 0)
+	if (idx == 0 || *h == NULL)
 		return (add_dnodeint(h, n));
-	
 	if (idx == count)
 		return (add_dnodeint_end(h, n));
-	
 	if (idx > count)
 	{
 		return (NULL);
 	}
-	
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
 	aux = *h;
 	for (i = 0 ; i < idx - 1; i++)
-			aux = aux->next;
+		aux = aux->next;
 	new->n = n;
 	new->next = aux->next;
 	new->prev = aux;
