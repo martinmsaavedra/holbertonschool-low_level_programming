87ff29dd6174e45623bbd97bb3221cfae1367936
martinmsaavedra <martin@bildung.uy>
2020-12-13 15:55:45 +0000

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *aux, *new;
+	dlistint_t *aux = NULL, *new = NULL;
 	unsigned int index = 0;
 
 	if (!h || !idx)
