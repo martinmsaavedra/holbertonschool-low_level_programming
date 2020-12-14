188ff7c531caffef1aeb073175222b1e7cc0c8ae
martinmsaavedra <martin@bildung.uy>
2020-12-14 18:46:22 +0000

    task 5

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/martinmsaavedra/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/make_me_win.so
-export LD_PRELOAD=/tmp/make_me_win.so
+wget -O ../libinject.so https://github.com/martinmsaavedra/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/librich.so
+export LD_PRELOAD=../librich.so ./gm
