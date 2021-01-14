24786217adee0949510244373f92ca7f8ca47465
martinmsaavedra <martin@bildung.uy>
2021-01-14 21:49:37 +0000

    Fix

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,10 +7,9 @@
   *Return: index of the hash table.
   */
 
-
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash, index;
+	unsigned long int hash = 0, index = 0;
 
 	hash = hash_djb2(key);
 	index = hash % size;
--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,8 +8,15 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int i = 0;
+	unsigned long int i = 0, j = 0;
 	hash_node_t *aux = NULL;
+	int count = 0;
+
+	for (; j < ht->size; j++)
+	{
+		if (ht->array[j] != NULL)
+			count++;
+	}
 
 	printf("{");
 	for (; i < ht->size; i++)
@@ -17,11 +24,14 @@ void hash_table_print(const hash_table_t *ht)
 		if (ht->array[i] != NULL)
 		{
 			aux = ht->array[i];
-			while (aux->next != NULL)
+			while (aux != NULL)
 			{
-				printf("\'%s\': \'%s\', ", aux->key, aux->value);
+				printf("\'%s\': \'%s\'", aux->key, aux->value);
+				if (!(count - 1 == 0 && aux->next == NULL))
+					printf(", ");
 				aux = aux->next;
 			}
+			count--;
 		}
 	}
 	printf("}\n");
