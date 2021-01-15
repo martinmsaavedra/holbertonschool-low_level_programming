a2f8e99733b99f68da9a8f4bec075b58590fcfad
martinmsaavedra <martin@bildung.uy>
2021-01-15 13:58:13 +0000

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,9 +10,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = NULL;
 	unsigned long int i;
-	
+
 	if (!size)
-		return NULL;
+		return (NULL);
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (!hash_table)
