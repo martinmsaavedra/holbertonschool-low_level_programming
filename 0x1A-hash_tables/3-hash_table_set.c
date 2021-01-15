99581404a4523465df2987addd3049a5fbf0fbb0
martinmsaavedra <martin@bildung.uy>
2021-01-15 14:25:46 +0000

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,13 +11,24 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node = NULL;
+	hash_node_t *new_node = NULL, *aux = NULL;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
+	aux = ht->array[index];
+
+	while (aux)
+	{
+		if (strcmp(key, aux->key) == 0)
+		{
+			aux->value = value;
+			return (1);
+		}
+		aux = aux->next;
+	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
