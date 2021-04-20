1338c0f1d715da745f29fa3d66ed1adbe99b29b3
Martin <martin@bildung.uy>
2021-04-20 00:31:41 +0000

    task 9

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,4 +1,5 @@
 #include "search_algos.h"
+#include <math.h>
 
 /**
  * jump_search - search a value using jump
@@ -26,21 +27,21 @@ int jump_search(int *array, size_t size, int value)
 	{
 		if (array[i] >= value)
 		{
-			printf("Value found between indexes [%zu] and [%zu]\n", i - jump, i);
+			printf("Value found between indexes [%ld] and [%ld]\n", i - jump, i);
 			for (j = (i - jump); j <= i; j++)
 			{
-				printf("Value checked array [%zu]: [%d]\n", j, array[j]);
+				printf("Value checked array [%ld]: [%d]\n", j, array[j]);
 				if (array[j] == value)
 					return (j);
 			}
 		}
 		else
-			printf("Value checked array [%zu]: [%d]\n", i, array[i]);
+			printf("Value checked array [%ld]: [%d]\n", i, array[i]);
 	}
 	if (array[size - 1] < value)
 	{
-		printf("Value found between indexes [%zu] and [%zu]\n", i - jump, i);
-		printf("Value checked array [%zu]: [%d]\n", i - jump, array[i - jump]);
+		printf("Value found between indexes [%ld] and [%ld]\n", i - jump, i);
+		printf("Value checked array [%ld]: [%d]\n", i - jump, array[i - jump]);
 	}
 	return (-1);
 }
