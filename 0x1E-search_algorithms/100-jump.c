24c6901eebd161ce4dcea56a4629e2310f736d25
Martin <martin@bildung.uy>
2021-04-20 23:54:15 +0000

    task 10

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -3,24 +3,24 @@
 
 /**
  * print_jump - auxiliary function that helps with printing.
- * @arr:  is a pointer to the first element of the array to search in.
- * @l: is the lower or beginning of the array.
- * @h:is the high part or the end.
- * @size: is the number of elements in array.
- * @value:  is the value to search for.
+ * @arr:  pointer to the first element of the array to search in.
+ * @start: start of the array.
+ * @end: end of the arraty.
+ * @size: number of elements in array.
+ * @value: value to search for.
  * Return: the first index where value is located. or -1
  */
 
-int print_jump(int *arr, int l, int h, int size, int value)
+int print_jump(int *arr, int start, int end, int size, int value)
 {
-	printf("Value found between indexes [%i] and [%i]\n", l, h);
-	if (h >= size)
-		h = size - 1;
-	for (; l <= h; l++)
+	printf("Value found between indexes [%i] and [%i]\n", start, end);
+	if (end >= size)
+		end = size - 1;
+	for (; start <= end; start++)
 	{
-		printf("Value checked array[%i] = [%i]\n", l, arr[l]);
-		if (arr[l] == value)
-			return (l);
+		printf("Value checked array[%i] = [%i]\n", start, arr[start]);
+		if (arr[start] == value)
+			return (start);
 	}
 	return (-1);
 }
@@ -28,15 +28,15 @@ int print_jump(int *arr, int l, int h, int size, int value)
  * jump_search - function that searches for a value in a sorted array of \
 integers using the Jump search algorithm
  * @array: is a pointer to the first element of the array to search in.
- * @size: is the number of elements in array.
- * @value: is the value to search for.
+ * @size: size  in array.
+ * @value: value to search for.
  * Return: the first index where value is located.
  */
 
 int jump_search(int *array, size_t size, int value)
 {
 	size_t jump = sqrt(size);
-	size_t i, l, h;
+	size_t i, start, end;
 
 	if (!array)
 		return (-1);
@@ -45,9 +45,9 @@ int jump_search(int *array, size_t size, int value)
 	{
 		if (array[i] >= value)
 		{
-			l = i - jump;
-			h = i;
-			return (print_jump(array, l, h, size, value));
+			start = i - jump;
+			end = i;
+			return (print_jump(array, start, end, size, value));
 		}
 		printf("Value checked array[%li] = [%i]\n", i, array[i]);
 		if (array[i] == value)
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,6 +7,7 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binary_search_aux(int *array, int value, int start, int end, int size);
 int jump_search(int *array, size_t size, int value);
+int print_jump(int *arr, int start, int end, int size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int int_search_aux(int *arr, int start, int end, size_t size, int value);
 int advanced_binary_aux(int *array, int value, int start, int end, int size);
