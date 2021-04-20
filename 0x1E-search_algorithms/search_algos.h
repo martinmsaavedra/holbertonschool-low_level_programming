97c02e1b6f4788c2b7055b4a4c61b8d254d0c081
Martin <martin@bildung.uy>
2021-04-20 00:42:37 +0000

    task 10

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,5 +8,5 @@ int binary_search(int *array, size_t size, int value);
 int binary_search_aux(int *array, int value, int start, int end, int size);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
-
+int int_search_aux(int *arr, int start, int end, size_t size, int value);
 #endif
