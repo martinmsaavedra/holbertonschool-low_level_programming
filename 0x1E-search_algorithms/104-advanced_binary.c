5801a7fc208a900f1495ebc7d591abb47a0f36e9
Martin <martin@bildung.uy>
2021-04-20 23:37:21 +0000

    Task 12

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,56 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - searches for a value in an array of integers
+ * @array: array to search.
+ * @size: size of the array.
+ * @value: value to search into array.
+ * Return: first index when value is located in array otherwise return -1.
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	int start = 0, end = (int)size - 1;
+
+	if (!array)
+		return (-1);
+
+	return (advanced_binary_aux(array, value, start, end, size));
+}
+
+/**
+ * advanced_binary_aux - searches for a value in an array of integers
+ * @array: array to search.
+ * @size: size of the array.
+ * @value: value to search into array.
+ * @start: start to search
+ * @end: end to search
+ * Return: first index when value is located in array otherwise return -1.
+ */
+
+int advanced_binary_aux(int *array, int value, int start, int end, int size)
+{
+	int mid = 0, i = 0;
+
+	printf("Searching in array: ");
+	for (i = start; i <= end; i++)
+	{
+		printf("%d", array[i]);
+		printf(i == end ? "\n" : ", ");
+	}
+	if (size == 0)
+		return (-1);
+
+	mid = start + (end - start) / 2;
+
+	if (array[mid] == value)
+		return (mid);
+	if (value > array[mid])
+		return (advanced_binary_aux(array,
+								  value, mid + 1, end, (size - 1) / 2));
+	if (value < array[mid])
+		return (advanced_binary_aux(array,
+								  value, start, mid - 1, (size - 1) / 2));
+
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,4 +9,6 @@ int binary_search_aux(int *array, int value, int start, int end, int size);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int int_search_aux(int *arr, int start, int end, size_t size, int value);
+int advanced_binary_aux(int *array, int value, int start, int end, int size);
+int advanced_binary(int *array, size_t size, int value);
 #endif
