631417ffac685bb21678da8949c37f1dcffd3873
Martin <martin@bildung.uy>
2021-04-21 14:46:17 +0000

    task 12

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -15,20 +15,19 @@ int advanced_binary(int *array, size_t size, int value)
 	if (!array)
 		return (-1);
 
-	return (advanced_binary_aux(array, value, start, end, size));
+	return (advanced_binary_aux(array, value, start, end));
 }
 
 /**
  * advanced_binary_aux - searches for a value in an array of integers
  * @array: array to search.
- * @size: size of the array.
  * @value: value to search into array.
  * @start: start to search
  * @end: end to search
  * Return: first index when value is located in array otherwise return -1.
  */
 
-int advanced_binary_aux(int *array, int value, int start, int end, int size)
+int advanced_binary_aux(int *array, int value, int start, int end)
 {
 	int mid = 0, i = 0;
 
@@ -38,19 +37,14 @@ int advanced_binary_aux(int *array, int value, int start, int end, int size)
 		printf("%d", array[i]);
 		printf(i == end ? "\n" : ", ");
 	}
-	if (size == 0)
+	if (end < start)
 		return (-1);
-
 	mid = start + (end - start) / 2;
-
-	if (array[mid] == value)
+	if (array[mid] == value && (mid == start || array[mid - 1] != value))
 		return (mid);
-	if (value > array[mid])
-		return (advanced_binary_aux(array,
-								  value, mid + 1, end, (size - 1) / 2));
-	if (value < array[mid])
-		return (advanced_binary_aux(array,
-								  value, start, mid - 1, (size - 1) / 2));
-
+	if (array[mid] >= value)
+		return (advanced_binary_aux(array, value, start, mid));
+	if (array[mid] < value)
+		return (advanced_binary_aux(array, value, mid + 1, end));
 	return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,6 +10,6 @@ int jump_search(int *array, size_t size, int value);
 int print_jump(int *arr, int start, int end, int size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int int_search_aux(int *arr, int start, int end, size_t size, int value);
-int advanced_binary_aux(int *array, int value, int start, int end, int size);
+int advanced_binary_aux(int *array, int value, int start, int end);
 int advanced_binary(int *array, size_t size, int value);
 #endif
